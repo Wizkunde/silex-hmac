044352334095d67f40db908b0108dc4b30f98fe0
Ron van der Molen <ron@wizkunde.nl>
2013-10-21 11:20:21 +0200

    [WIP] Hmac validation middleware improvements

--- src/Wizkunde/Provider/HmacServiceProvider.php
+++ src/Wizkunde/Provider/HmacServiceProvider.php
@@ -4,7 +4,7 @@ namespace Wizkunde\Provider;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 use Symfony\Component\HttpFoundation\Request;
-
+use Wizkunde\Traits\ErrorTrait;
 use Wizkunde\Service\HmacService;
 
 /**
@@ -13,6 +13,8 @@ use Wizkunde\Service\HmacService;
  */
 class HmacServiceProvider implements ServiceProviderInterface
 {
+    use ErrorTrait;
+
     /**
      * {@inheritDoc}
      */
@@ -32,21 +34,29 @@ class HmacServiceProvider implements ServiceProviderInterface
                 return new HmacService($app['validator'], $app['request']);
         });
 
-        /**
-         * Add the middleware to the application
-         */
-        $app->before(function (Request $request, Application $app) {
+        // creates a new controller based on the default route
+        $controllers = $app['controllers_factory'];
+        $controllers->before($this->getHmacMiddleware($app));
+    }
+
+    /**
+     * Get the middleware for hmac validation
+     * 
+     * @param Application $app
+     * @return callable
+     */
+    private function getHmacMiddleware(Application $app)
+    {
+        return function (Request $request, Application $app) {
             $app['service.hmac']->validate(
-                array('app' => $app)
+                array('app' => $app),
+                $app['request']->getContent()
             );
 
             if($app['service.hmac']->hasErrors()) {
-                foreach ($app['service.hmac']->getErrors() as $error) {
-                    $errorResponse[] = $error->getPropertyPath().' '.$error->getMessage()."\n";
-                }
-
-                return new JsonResponse(array('errors' => $errorResponse));
+                $this->setErrors($app['service.hmac']->getErrors());
+                return new JsonResponse(array('errors' => $this->getErrorResponse()));
             }
-        });
+        };
     }
 }
