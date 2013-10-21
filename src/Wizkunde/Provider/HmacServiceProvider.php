c6be4091acf002b6f22e6b6ed6c27c57102a6776
Ron van der Molen <ron@wizkunde.nl>
2013-10-21 10:57:40 +0200

    [FEATURE] Added before hook to service provider

--- src/Wizkunde/Provider/HmacServiceProvider.php
+++ src/Wizkunde/Provider/HmacServiceProvider.php
@@ -29,6 +29,24 @@ class HmacServiceProvider implements ServiceProviderInterface
          */
         $app['service.hmac'] = $app->share(function($app) {
                 return new HmacService($app['validator'], $app['request']);
-            });
+        });
+
+        /**
+         * Add the middleware to the application
+         */
+        $app->before(function (Request $request, Application $app) {
+            $app['service.hmac']->validate(
+                $request->attributes->get('section'),
+                $request->getContent()
+            );
+
+            if($app['service.hmac']->hasErrors()) {
+                foreach ($app['service.hmac']->getErrors() as $error) {
+                    $errorResponse[] = $error->getPropertyPath().' '.$error->getMessage()."\n";
+                }
+
+                return new JsonResponse(array('errors' => $errorResponse));
+            }
+        });
     }
 }
---
+++ tests/Wizkunde/Validator/Constraints/HmacValidatorTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace Wizkunde\Validator\Constraints;
+
+use Wizkunde\Traits\SilexMock;
+
+class HmacServiceTest extends \PHPUnit_Framework_TestCase
+{
+    // Implement the mocking interface for a silex applicationx
+    use SilexMock;
+
+    public function testCheckIfHeadersComplete()
+    {
+
+    }
+}
